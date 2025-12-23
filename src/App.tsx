6f209f2307c1a7c24e4f22454cf265cf09aac01b
Khendon <anton.bringner@gmail.com>
2025-12-23 17:09:55 +0100

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Routes>
+      <Navbar/>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/contact" element={<Contact />} />

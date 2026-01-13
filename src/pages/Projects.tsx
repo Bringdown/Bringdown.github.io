13c0b3ac9de842ccc4b585d8f92c8d3d6c51a439
Khendon <anton.bringner@gmail.com>
2026-01-13 16:41:04 +0100

    Improved project card & title placement

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,10 +3,10 @@ import { projects } from "../data/projects";
 
 const Projects = () => {
   return (
-    <main className="py-24">
-      <div>
-        <h1 className="text-4xl font-semibold mb-12">Projects</h1>
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+    <main className="py-15">
+      <div className="max-w-5xl mx-auto px-4">
+        <h1 className="text-center text-4xl font-semibold mb-12">Projects</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 justify-items-center">
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
